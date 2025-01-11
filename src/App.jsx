64d1dd1f00dcee8f50e77e4b9dd4cdf146ebeb55
BONDdv <natchapon.thongmag@gmail.com>
2025-01-11 17:09:36 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react"
+import ProductList from "./components/productList";
+import ProductForm from "./components/productForm";
+
+function App() {
+
+  const [popup, setPopup] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("")
+  const [products, setProducts] = useState([])
+  const [filterProducts, setFilterProducts] = useState(products);
+  const [isLoading, setIsLoading] = useState(false);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+
+  useEffect(()=>{
+    fetchProducts();
+  }, [])
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/products`);
+      if(!response) {
+        throw new Error("ไม่สามารถดึงข้อมูลสินค้าได้");
+      }
+      const data = await response.json();
+      setProducts(data.products);
+      setFilterProducts(data.products);
+    } catch(error) {
+      console.log("Error in fetch product", error);
+    }
+  }
+
+  useEffect(()=> {
+    setFilterProducts(products);
+  }, [products]);
+
+  const hdlSearch = () => {
+    setIsLoading(true);
+    setTimeout( async ()=> {
+      
+      try {
+        const response = await fetch(`${apiUrl}/products/search?query=${searchQuery}`)
+        if(!response) {
+          throw new Error('ไม่สามารถค้นหาสินค้าได้')
+        }
+        const data = await response.json();
+        
+        setFilterProducts(data);
+      } catch (error) {
+        console.log("error in search product", error)
+      } finally {
+        setIsLoading(false);
+      }
+    }, 3000);
+  };
+
+  const hdlClearSearch = () => {
+    setSearchQuery("");
+    setFilterProducts(products);
+  }
+
+  
+  return (
+    <>
+      <div>
+        <h2>ระบบจัดการข้อมูลสินค้า</h2>
+
+        <div>
+          <form className='search' onSubmit={(e) => {
+            e.preventDefault();
+            hdlSearch();
+          }}>
+          <input 
+            type="text" 
+            placeholder="ค้นหาสินค้า.." 
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          /> 
+          <button type="submit" className="searchBtn"><i className="fa fa-search"></i></button>
+
+          { searchQuery && (
+            <button type="button" className="clearBtn" onClick={hdlClearSearch}>X</button>
+          )}
+          </form>
+        </div>
+
+        <div className="productAdd">
+          <h3>รายการสินค้า</h3>
+
+          <button className="addButton" onClick={()=> setPopup(true)}>เพิ่มสินค้า</button>
+          
+          {popup && (
+            <ProductForm 
+              currentProduct={currentProduct}
+              setCurrentProduct={setCurrentProduct}
+              products={products}
+              setProducts={setProducts}
+              setPopup={setPopup}
+            />
+          )}
+        </div>
+
+          <ProductList 
+            isLoading={isLoading}
+            filterProducts={filterProducts}
+            setPopup={setPopup}
+            setCurrentProduct={setCurrentProduct}
+            setProducts={setProducts}
+          />
+
+      </div>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+
+const ProductForm = ({
+    currentProduct,
+    setCurrentProduct,
+    products,
+    setProducts,
+    setPopup,
+}) => {
+    const [name, setName] = useState(currentProduct ? currentProduct.name : "");
+    const [price, setPrice] = useState(currentProduct ? currentProduct.price : "");
+    const [stock, setStock] = useState(currentProduct ? currentProduct.stock : "");
+    const [error, setError] = useState("");
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+
+    useEffect(() => {
+        if(currentProduct) {
+            setName(currentProduct.name);
+            setPrice(currentProduct.price);
+            setStock(currentProduct.stock);
+        }
+    }, [currentProduct]);
+
+
+    const hdlSubmit = async (e) => {
+        e.preventDefault();
+
+        if (products.some(product => product.name === name )) {
+            setError("สินค้ามีอยู่ในระบบแล้ว");
+            return; 
+        }
+
+        
+        const newProduct = {name, price: parseFloat(price), stock: parseInt(stock) };
+        const updateProduct = {name, price: parseFloat(price), stock: parseInt(stock) };
+
+        if (currentProduct) {
+          try {
+            const response = await fetch(`${apiUrl}/products/${currentProduct.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(updateProduct),
+            })
+            if (!response.ok) {
+                throw new Error("ไม่สามารถแก้ไขสินค้าได้");
+            }
+
+            const data = await response.json();
+            setProducts(
+                products.map((product) => 
+                    product.id === currentProduct.id
+                        ? {...product, ...updateProduct}
+                        : product
+                )
+            )
+          } catch (error) {
+            console.error("ข้อผิดพลาดในการเเก้ไขสินค้า", error)
+          }
+          setCurrentProduct(null);
+        } else {
+            try {
+                const response = await fetch(`${apiUrl}/products`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type" : "application/json",
+                    },
+                    body: JSON.stringify(newProduct),
+                })
+                if (!response.ok) {
+                    throw new Error("ไม่สามารถเพิ่มสินค้าได้");
+                  }
+                
+                const data = await response.json();
+                setProducts([...products, data.product])
+            } catch (error) {
+                console.error("ข้อผิดพลาดในการเพิ่มสินค้า", error)
+            }
+        }
+        setPopup(false);
+      }
+
+
+  
+    
+      return (
+        <div className="popupOverlay active" onClick={()=> {
+            setPopup(false)
+            setCurrentProduct(null)}
+            }>
+            <div className="popupContent" onClick={(e)=> e.stopPropagation()}>
+              <button className="closeBtn" onClick={()=> {
+                setPopup(false)
+                setCurrentProduct(null)}
+                }>x</button>
+              <h2>{currentProduct ? 'แก้ไขสินค้า' : "เพิ่มรายการสินค้า"}</h2>
+              <form onSubmit={hdlSubmit}>
+                <label>ชื่อสินค้า: </label>
+                {error && <div style={{ color: 'red', marginTop: '5px' }}>{error}</div>}
+                <input 
+                  type="text" 
+                  name="name"
+                  value={name}
+                  onChange={(e)=> setName(e.target.value)}
+                  required
+                />
+    
+                <label>ราคาสินค้า(บาท):</label>
+                <input 
+                  type="number"
+                  name="price"
+                  value={price}
+                  onChange={(e)=> setPrice(e.target.value)}
+                  required
+                />
+    
+                <label>จำนวน:</label>
+                <input 
+                  type="number" 
+                  name="stock"
+                  value={stock}
+                  onChange={(e)=> setStock(e.target.value)}
+                  required
+                />
+                
+                <button type="submit">ยืนยัน</button>
+                <button 
+                  type="button"
+                  className="cancel" 
+                  onClick={()=> {
+                    setPopup(false)
+                    setCurrentProduct(null)
+                  }} 
+                >ยกเลิก</button>
+              </form>
+            </div>
+          </div>
+      )
+    
+}
+
+export default ProductForm;
+
